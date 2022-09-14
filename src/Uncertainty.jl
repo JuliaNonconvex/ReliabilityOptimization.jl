6cd975ee16c358ecea384062ae3616ad64499ae9
Lucas Pereira <lucas.lmspereira@gmail.com>
2022-09-14 10:27:43 -0300

    Updates

--- src/Uncertainty.jl
+++ src/Uncertainty.jl
@@ -1,7 +1,36 @@
 module Uncertainty
 
-struct RandomFunction
-  field
+function FORM()
+
+end
+
+function RIA()
+
+end
+
+function getp0(f, x)
+  
+	@unPack forward, params, conds, method = f
+  return ImplicitFunction(forward, (x, p) -> conds, method)(x, params)
+
+end
+
+struct RandomFunction{F,P}
+  f::F
+  p::P
+	conditions::F
+	method::F
+end
+
+function (f::RandomFunction)(x)
+	mup = mean(f.p)
+	covp = cov(f.p)
+	p0 = getp0(f, x) # should use ImplicitFunctions to be differentiable and efficient
+	dfdp0 = Zygote.jacobian(p -> f.f(x, p), p0)[1]
+	fp0 = f.f(x, p0)
+	muf = fp0 + dfdp0 * (mup - p0)
+	covf = dfdp0 * covp * dfdp0'
+	return MvNormal(muf, covf)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,18 @@
-using Uncertainty, Test, ImplicitDifferentiation, Zygote, TopOpt, ChainRulesCore
+using Uncertainty, Test, ImplicitDifferentiation, Zygote, TopOpt, ChainRulesCore, UnPack
 
 @testset "Uncertainty.jl" begin
     ## 1) ImplicitDifferentiation.jl -> ensure differentiability
-    zero_gradient(x, y) = 2(y - x) # place-holders from ImplicitDifferentiation.jl docs
-    implicit = ImplicitFunction(???, zero_gradient)
-    x = rand(3)
-    implicit(x)
-    Zygote.jacobian(implicit, x)[1]
     ## 2) solve optimization -> linear approximation around p0 (MPP)
-    # ???
     ## 3) linear approximation around p0 and base distribution of parameters -> RandomFunction struct
-    E_p(f(x; p)) = f(x; p = p0) + df(x; p = p0)/dp * mu_p - df(x; p = p0)/dp * p0
-    p ~ MvNormal(mu_p, cov_p)
-    mu(x) = f(x; p = p0(x)) + df(x; p = p0(x))/dp * mu_p - df(x; p = p0(x))/dp * p0
-    cov(x) = (df(x; p = p0)/dp) * cov_p * (df(x; p = p0)/dp)'
-    f(x; p) ~ MvNormal(mu(x), cov(x))
-    rf = RandomFunction(f, MvNormal(zeros(3), I(3)), method = FORM(RIA(g)))
     ## 4) RandomFunction struct -> mean and cov functions
+    ## 5) obj(x) to be used in actual application (e.g. TO)
+    function myFunc(x; p)
+    end
+    function g()
+    end
+    rf = RandomFunction(f, MvNormal(zeros(3), I(3)), g, method = FORM(RIA(g)))
     function obj(x)
         dist = rf(x)
         mean(dist) + 2 * std(dist)
     end
-    ## 5) obj(x) to be used in actual application (e.g. TO)
-    # TopOpt.jl code
 end
