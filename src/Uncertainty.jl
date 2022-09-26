7491c2829154f080867bc5ff80b58295838c3c8d
Lucas Pereira <lucas.lmspereira@gmail.com>
2022-09-26 08:48:16 -0300

    work on getp0

--- src/Uncertainty.jl
+++ src/Uncertainty.jl
@@ -10,13 +10,28 @@ end
 
 function getp0(randFunc::RandomFunction, x)
   @unpack f, p, conditions, method = randFunc
-	innerOptModel = Nonconvex.Model(f) # Wrap original function f in Nonconvex.jl optimization model
-	KKT = [p_ -> Zygote.gradient(p -> cond(x, p), p_)[1] for cond in conditions] # formulate KKT conditions
-	[add_eq_constraint!(innerOptModel, KKTᵢ) for KKTᵢ in KKT] # add KKTs to optimization model
-	implicit = ImplicitFunction( # implicit wrapper to ensure differentiability
-		optimize(innerOptModel, IpoptAlg(), mean.(p), options = IpoptOptions()),
-		KKT, method)
-	return implicit(x)
+	lag_multipliers = 0.0
+	function forward(x)
+		obj = get_obj(x) # gets an objective function of p
+		constr = get_constr(x) # gets the constraint on p
+		# solve the RIA problem to find p0
+		innerOptModel = Nonconvex.Model(obj)
+		add_eq_constraint!(innerOptModel, constr)
+		result = optimize(innerOptModel, IpoptAlg(), mean.(p), options = IpoptOptions())
+		p0 = result.minimizer
+		# get the Lagrangian multipliers and store them in lag_multipliers
+		grads = Zygote.gradient((x, p0) -> f(x, p), [x, p0])[1]
+		jac = Zygote.jacobian((x, p0) -> conditions(x, p), [x, p0])[1]
+		lag_multipliers = grads*inv(jac)
+		return p0
+	end
+	function kkt_conditions(x, p)
+		obj = get_obj(x) # gets an objective function of p
+		constr = get_constr(x) # gets the constraint on p
+		return Zygote.gradient(obj, p) + Zygote.jacobian(constr, p)' * lag_multipliers
+	end
+	implicit_f = ImplicitFunction(forward, kkt_conditions)
+	return implicit_f(x)
 end
 
 struct RandomFunction{F,P}
