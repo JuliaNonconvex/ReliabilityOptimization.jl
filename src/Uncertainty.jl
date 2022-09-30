f86e0c707e2a76619d6afa20f4eb19fb2e028483
Mohamed Tarek <mohamed82008@gmail.com>
2022-09-30 23:08:55 +1000

    fix implementation and tests

--- src/Uncertainty.jl
+++ src/Uncertainty.jl
@@ -1,36 +1,15 @@
 module Uncertainty
 
-using ImplicitDifferentiation, Zygote, TopOpt, ChainRulesCore, LinearAlgebra
-using UnPack, Nonconvex, Statistics, GLMakie, Distributions, Reexport
-Nonconvex.@load Ipopt
+using ImplicitDifferentiation, Zygote, LinearAlgebra, ChainRulesCore, ForwardDiff
+using UnPack, NonconvexIpopt, Statistics, Distributions, Reexport, DistributionsAD
 @reexport using LinearAlgebra
+@reexport using Statistics
+export RandomFunction, FORM, RIA, MvNormal
 
-struct RandomFunction{F,P}
+struct RandomFunction{F,P,M}
   f::F
   p::P
-	method
-end
-
-function getp0(randFunc::RandomFunction, x)
-  @unpack f, p, method = randFunc
-	lag_multipliers = 0.0
-	function forward(x)
-		obj = p -> p'*p # gets an objective function of p
-		constr = p -> f(x, p) # gets the constraint on p
-		# solve the RIA problem to find p0
-		innerOptModel = Nonconvex.Model(obj)
-		addvar!(innerOptModel, zeros(size(p)), fill(10.0, size(p)))
-		add_eq_constraint!(innerOptModel, constr)
-		result = optimize(innerOptModel, IpoptAlg(), mean(p), options = IpoptOptions())
-		lag_multipliers = result # get the Lagrangian multipliers
-		return result.minimizer
-	end
-	function kkt_conditions(x, p)
-		constr = p -> f(x, p) # gets the constraint on p
-		return 2 * p + Zygote.gradient(constr, p)[1] * lag_multipliers
-	end
-	implicit_f = ImplicitFunction(forward, kkt_conditions)
-	return implicit_f(x)
+  method::M
 end
 
 struct FORM{M}
@@ -38,24 +17,79 @@ struct FORM{M}
 end
 struct RIA end
 
-function RandomFunction(f, p)
-	return RandomFunction(f, p, FORM(RIA()))
+function get_forward(f, p, ::FORM{<:RIA})
+	function forward(x)
+		# gets an objective function of p
+		obj = pc -> begin
+		   _p = pc[1:end-1]
+		   c = pc[end]
+		   return _p'*_p + c^2
+	   end
+	   # gets the constraint on p
+	   constr = pc -> begin
+		   _p = pc[1:end-1]
+		   c = pc[end]
+		   f(x, _p) .- c
+	   end
+	   # solve the RIA problem to find p0
+	   # should use and reuse the VecModel
+	   innerOptModel = Model(obj)
+	   n = size(p)[1]
+	   addvar!(innerOptModel, fill(-Inf, n + 1), fill(Inf, n + 1))
+	   add_eq_constraint!(innerOptModel, constr)
+	   result = optimize(innerOptModel, IpoptAlg(), [mean(p); 0.0], options = IpoptOptions(print_level = 0))
+	   return vcat(result.minimizer, result.problem.mult_g[1])
+   end
+   return forward
 end
-RandomFunction(f, p; method) = RandomFunction(f, p, method)
+
+# this is inefficient for multiple reasons
+# first is the use of ForwardDiff because Zygote over Zygote fails
+# second is that we are taking the jacobian wrt to both x and p because
+# of a limitation in Zygote over ForwardDiff
+function jac2(f, x, p)
+	ForwardDiff.jacobian(
+		xp -> f(xp[1:length(x)], xp[length(x)+1:end]),
+		vcat(x, p),
+	)[:,length(x)+1:end]
+end
+
+function get_conditions(f, ::FORM{<:RIA})
+	function kkt_conditions(x, pcmult)
+		p = pcmult[1:end-2]
+		c = pcmult[end-1]
+		mult = pcmult[end]
+		return vcat(2 * p + jac2(f, x, p)' * mult, 2c - mult, f(x, p) .- c)
+	end
+end
+
+function get_implicit(f, p, method)
+	forward = get_forward(f, p, method)
+	kkt_conditions = get_conditions(f, method)
+	return ImplicitFunction(forward, kkt_conditions)
+end
+
+function getp0(f, x, p, method::FORM{<:RIA})
+	implicit_f = get_implicit(f, p, method)
+	return implicit_f(x)[1:size(p)[1]]
+end
+
+function RandomFunction(f, p; method = FORM(RIA()))
+	return RandomFunction(f, p, method)
+end
+
+_vec(x::Real) = [x]
+_vec(x) = x
 
 function (f::RandomFunction)(x)
 	mup = mean(f.p)
 	covp = cov(f.p)
-	p0 = getp0(f, x) # should use ImplicitFunctions to be differentiable and efficient
-	dfdp0 = Zygote.jacobian(p -> f.f(x, p), p0)[1]
+	p0 = getp0(f.f, x, f.p, f.method)
+	dfdp0 = jac2(f.f, x, p0)
 	fp0 = f.f(x, p0)
-	muf = fp0 + dfdp0 * (mup - p0)
+	muf = _vec(fp0) + dfdp0 * (mup - p0)
 	covf = dfdp0 * covp * dfdp0'
 	return MvNormal(muf, covf)
 end
-
-export RandomFunction
-export FORM
-export MvNormal
 
 end
